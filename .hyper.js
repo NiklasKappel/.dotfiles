48b178654e1a3384cabfe9cf78909f72be8bf601
Niklas Kappel <niklas.kappel@senacor.com>
2025-04-09 11:32:55 +0200

    Setup dev env

--- .hyper.js
+++ .hyper.js
@@ -10,7 +10,7 @@ module.exports = {
         // default font size in pixels for all tabs
         fontSize: 12,
         // font family with optional fallbacks
-        fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+        fontFamily: '"DejaVuSansM Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
         // default font weight: 'normal' or 'bold'
         fontWeight: 'normal',
         // font weight for bold characters: 'normal' or 'bold'
@@ -130,6 +130,11 @@ module.exports = {
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
         // for advanced config flags please refer to https://hyper.is/#cfg
+
+	verminal: {
+          fontFamily: '"DejaVuSansM Nerd Font"',
+	  fontSize: 14
+	}
     },
     // a list of plugins to fetch and install from npm
     // format: [@org/]project[#version]
