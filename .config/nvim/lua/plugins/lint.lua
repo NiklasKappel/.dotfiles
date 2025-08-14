dc8b1d17ae4fe16eeeb1326b45283fd71d05ad49
Niklas Kappel <niklas.kappel@senacor.com>
2025-08-14 15:07:25 +0200

    Add prettier, eslint, install ts_ls globally

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -2,7 +2,17 @@ return {
 	"mfussenegger/nvim-lint",
 	config = function()
 		local lint = require("lint")
-		lint.linters_by_ft = {}
+		lint.linters_by_ft = {
+			css = { "eslint" },
+			javascript = { "eslint" },
+			javascriptreact = { "eslint" },
+			json = { "eslint" },
+			json5 = { "eslint" },
+			jsonc = { "eslint" },
+			markdown = { "eslint" },
+			typescript = { "eslint" },
+			typescriptreact = { "eslint" },
+		}
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			callback = function()
 				lint.try_lint()
--- .config/nvim/lua/plugins/mason-tool-installer.lua
+++ .config/nvim/lua/plugins/mason-tool-installer.lua
@@ -14,6 +14,7 @@ return {
 			"lua_ls",
 			"marksman",
 			"texlab",
+			"ts_ls",
 
 			-- formatters
 			"stylua",
