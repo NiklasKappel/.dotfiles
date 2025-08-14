bd4b0e0f75487e63b1c1ab2e22ec7e973b12593c
Niklas Kappel <niklas.kappel@senacor.com>
2025-08-14 13:48:35 +0200

    Add stow script

---
+++ run_stow.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Change to that directory
+cd "$DIR" || exit
+
+# Run the stow command
+stow --no-folding --adopt .
