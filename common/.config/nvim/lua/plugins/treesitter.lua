1a62ea033c1a378d9f7b4fd2712858db2c2565ae
Niklas Kappel <niklas@kappel.com>
2026-02-03 09:14:42 +0100

    Update

--- common/.config/nvim/lua/plugins/treesitter.lua
+++ common/.config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,36 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	init = function(plugin)
-		require("lazy.core.loader").add_to_rtp(plugin)
-		require("nvim-treesitter.query_predicates")
-	end,
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			highlight = {
-				enable = true,
-			},
 			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					scope_incremental = [[\]],
-					node_decremental = "<BS>",
-				},
+			highlight = { enable = true },
+			folds = { enable = true },
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"printf",
+				"python",
+				"query",
+				"regex",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
 			},
 		})
 	end,
