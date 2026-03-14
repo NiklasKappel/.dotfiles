b0c2fd69782ef166e4a327c738fb59b0fc4d82d8
Niklas Kappel <niklas@kappel.com>
2026-03-14 16:46:48 +0100

    Use stable nix and clean up

--- linux/.config/home-manager/flake.nix
+++ linux/.config/home-manager/flake.nix
@@ -1,11 +1,10 @@
 {
-  description = "Home Manager configuration of niklas";
+  description = "Home Manager configuration";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -20,12 +19,7 @@
       homeConfigurations."niklas" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
