ad7cd7a95ba0e22b7811f066dc1a1a287f333594
Niklas Kappel <niklas@kappel.com>
2026-04-18 12:46:24 +0200

    Remove nix

--- linux/.config/fish/functions/hm.fish
+++
@@ -1,4 +0,0 @@
-function hm --wraps='home-manager switch --flake ~/.dotfiles_nix/' --description 'alias hm=home-manager switch --flake ~/.dotfiles_nix/'
-  home-manager switch --flake ~/.dotfiles_nix/ $argv
-        
-end
